6ff2048951bf1f86785e273fc237c71dcddb55fa
Navi-firm <kuria.wa.kinuthia@gmail.com>
2018-05-23 08:27:27 +0300

    Backup ya Backup

---
+++ application/views/backend/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; 2015 <strong>Masomo Connect</strong>. 
+    Powered by 
+	<a href="http://getcloudrack.com" 
+    	target="_blank">Cloud Rack</a>
+</footer>
